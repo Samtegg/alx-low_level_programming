8c912561d8612facdc7e6a2b8b2c3c962bd1fed2
samtegg <osimesimon@gmail.com>
2023-10-06 16:44:24 -0700

    d

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,22 +2,22 @@
 
 /**
  * main - This would print the numbers between 
- * 0 to 9  and the letters between a to f.
+ * 0 to 9 and the letters between a to f.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+    int i;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	for (i = 97; i < 103; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
+    for (i = 48; i < 58; i++)
+    {
+        putchar(i);
+    }
+    for (i = 97; i < 103; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
 }
