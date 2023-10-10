3e140940464fdb029667138a84fd3b4353c24f19
samtegg <osimesimon@gmail.com>
2023-10-10 16:54:08 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -This will add two integers then returns the result
+ * @a: int added to b
+ * @b: int added to a
+ * Return: total of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - This will compute the absolute value of the integer
+ * @n: int to be checked
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
