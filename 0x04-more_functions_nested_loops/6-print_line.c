7a7a47ea8fe0b8fc9ec51acdcc4fb1a9b3b38484
samtegg <osimesimon@gmail.com>
2023-10-12 15:06:45 -0700

    d

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - this draws a line in the terminal
+ * @n: num of times the char _ should be printed
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
