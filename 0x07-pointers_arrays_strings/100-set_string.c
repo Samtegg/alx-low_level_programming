e11a56cf6e1af5b9c784301b1bd1d19ac35dec54
samtegg <osimesimon@gmail.com>
2023-10-23 15:17:12 -0700

    d

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - this the main function
+  *
+  * @s: the function parameter
+  *
+  * @to: the function parameter
+  *
+  * Return: its always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
