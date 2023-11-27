a789652b8c959b66f24afd32ced7379ec7d1a9ed
samtegg <osimesimon@gmail.com>
2023-11-27 15:29:03 -0800

    d

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	for (dnletters = 0; text_content[dnletters]; dnletters++)
 		;
 
-	drwr = write(dfd, text_content,dnletters);
+	drwr = write(dfd, text_content, dnletters);
 
 	if (drwr == -1)
 		return (-1);
